693536238f2e8c834c85b54122e9f7c8d0a18648
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2023-08-05 06:36:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let encoded = window.btoa(text);
 document.getElementById("cyrust").innerHTML = "Original: " + text + "<br>Password: " + encoded;
 
 const downloadBtn = document.querySelector(".download-btn");
-const fileLink1 = "";
 const fileLink = "https://drive.google.com/uc?export=download&id=1aYiaLn3YOjL-_o5QBCy7tU1epqA6gZoi";
 
 const initTimer = () => {
@@ -24,8 +23,9 @@ const initTimer = () => {
             return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
         }
         clearInterval(initCounter);
-    
-        downloadBtn.innerText = `encoded`;
+        location.href = fileLink;
+        downloadBtn.innerText = "Your file is downloading...";
+        let encoded = window.btoa(text);
         setTimeout(() => {
             downloadBtn.classList.replace("timer", "disable-timer");
             downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
