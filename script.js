b59a1e08df12fb6379a4bfa10448fd99b4e73acd
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2023-08-05 18:49:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-id = cyrust;
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-let text = "FF Tools ProMax " + d + "/" + m + "/" + y;
-let encoded = window.btoa(text);
-cyrust.innerHTML = "Original: " + text + "<br>Password: " + encoded;
-
-
 const downloadBtn = document.querySelector(".download-btn");
 const fileLink = "https://drive.google.com/uc?export=download&id=1aYiaLn3YOjL-_o5QBCy7tU1epqA6gZoi";
 
 const initTimer = () => {
     if(downloadBtn.classList.contains("disable-timer")) {
-        
+        return location.href = fileLink;
     }
     let timer = downloadBtn.dataset.timer;
     downloadBtn.classList.add("timer");
-    downloadBtn.innerHTML = `Password<b>${timer}</b> seconds`;
+    downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
     const initCounter = setInterval(() => {
         if(timer > 0) {
             timer--;
-            return downloadBtn.innerHTML = `Password<b>${timer}</b> seconds`;
+            return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
         }
         clearInterval(initCounter);
-
-
-        
+        location.href = fileLink;
         downloadBtn.innerText = "Your file is downloading...";
         setTimeout(() => {
             downloadBtn.classList.replace("timer", "disable-timer");
