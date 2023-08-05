cbcff2c937a06674af7f1ca961dbb910415bf1f8
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2023-08-05 13:34:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const downloadBtn = document.querySelector(".download-btn");
 const fileLink = "https://drive.google.com/uc?export=download&id=1aYiaLn3YOjL-_o5QBCy7tU1epqA6gZoi";
-
+const text = "test";
 const initTimer = () => {
     if(downloadBtn.classList.contains("disable-timer")) {
         return text = fileLink;
